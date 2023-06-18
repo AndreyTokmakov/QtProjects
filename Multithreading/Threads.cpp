ec0fa2fb70d569559a56c987d39a7528f5559b3d
andtokm <andrei.tokmakov@tii.ae>
2023-06-18 21:44:56 +0400

    18_06_2023__2

--- Multithreading/Threads.cpp
+++ Multithreading/Threads.cpp
@@ -90,10 +90,120 @@ namespace Threads::DemoTwo
     }
 }
 
+namespace Threads::Pool
+{
+    class Work : public QRunnable {
+    public:
+        void run() override {
+            qDebug() << "Hello from thread " << QThread::currentThread();
+        }
+    };
+
+    int Test(int argc, char *argv[])
+    {
+        QCoreApplication app(argc, argv);
+        Work work;
+        work.setAutoDelete(false);
+        QThreadPool *threadPool = QThreadPool::globalInstance();
+        threadPool->start(&work);
+        qDebug() << "hello from GUI thread " << QThread::currentThread();
+        threadPool->waitForDone();
+        return EXIT_SUCCESS;
+    }
+}
+
+namespace Threads::ClockGUI
+{
+
+    class ClockThread : public QThread
+    {
+    Q_OBJECT
+    signals:
+        void sendTime(QString time);
+
+    private:
+        void run() override
+        {
+            QTimer timer;
+            connect(&timer, SIGNAL(timeout()), this, SLOT(timerHit()), Qt::DirectConnection);
+            timer.setInterval(10);
+            timer.start();   // puts one event in the threads event queue
+            exec();
+            timer.stop();
+        }
+
+        QString m_lastTime;
+
+    private slots:
+
+        void timerHit()
+        {
+            QString newTime= QDateTime::currentDateTime().toString("ddd MMMM d yy, hh:mm:ss");
+            if(m_lastTime != newTime ){
+                m_lastTime = newTime;
+                emit sendTime(newTime) ;
+            }
+        }
+    };
+
+    int runClock(int argc, char *argv[])
+    {
+        QApplication app(argc, argv);
+        QWidget widget;
+        QLabel *label = new QLabel;
+        QHBoxLayout *layout = new QHBoxLayout(&widget);
+        layout->addWidget(label);
+        widget.setWindowTitle("clock");
+
+        ClockThread clockThread;
+        QObject::connect(&clockThread,
+                         SIGNAL(sendTime(QString)),
+                         label,
+                         SLOT(setText(QString)),
+                         Qt::QueuedConnection);
+        clockThread.start();
+        widget.show();
+        QApplication::exec();
+
+        clockThread.quit();
+        clockThread.wait();
+
+        return EXIT_SUCCESS;
+    }
+}
+
+/*
+namespace Threads::PermanentThread
+{
+    int main(int argc, char *argv[])
+    {
+        QCoreApplication app(argc, argv);
+        Thread thread;
+        qDebug() << "main thread ID: " << app.thread()->currentThreadId();
+        WorkerObject *worker = new WorkerObject;
+        thread.launchWorker(worker);
+        QMetaObject::invokeMethod(worker, "doWork", Qt::QueuedConnection);
+        QMetaObject::invokeMethod(worker, "startPolling",  Qt::QueuedConnection,  Q_ARG(int, 500));
+        //let application produce output for 3 seconds and quit
+        QTimer::singleShot(3000, &app, SLOT(quit()));
+        app.exec();
+        thread.stop();
+        thread.wait();
+        delete worker;
+        return 0;
+    }
+}
+*/
+
+
 void Threads::TestAll(int argc, char *argv[])
 {
     // DemoOne::SimpleThread(argc, argv);
-    DemoTwo::Test(argc, argv);
+    // DemoTwo::Test(argc, argv);
+
+    // Pool::Test(argc, argv);
+
+    ClockGUI::runClock(argc, argv);
 }
 
 #include "Threads.moc"
